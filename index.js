0c84fd030619347db02e636a861d4e282526b52b
Eddie Madrigal <edmadrigal@yahoo.com>
2020-05-03 18:44:10 -0700

    server.js refactored

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const server = require('./api/server');
-const port = 5000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log(`\n** Running on ${port}`);
+  console.log(`\n** Running on port: ${port}`);
 });
